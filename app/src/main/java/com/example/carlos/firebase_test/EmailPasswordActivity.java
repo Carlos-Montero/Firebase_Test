f4749881d398d0650a53148d0755a4195a92b80e
Carlos Montero <carlosmonteroreina@gmail.com>
2019-04-17 11:23:55 +0200

    Segundo commit. Ya funciona la autenticación con mail y password utilizando Firebase. Para probarlo, se ha definido como activity principal la EmailPasswordActivity.java en el Manifest.

--- app/src/main/java/com/example/carlos/firebase_test/EmailPasswordActivity.java
+++ app/src/main/java/com/example/carlos/firebase_test/EmailPasswordActivity.java
@@ -1,11 +1,22 @@
 package com.example.carlos.firebase_test;
-import android.support.annotation.NonNull;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+//QUÉ IMPORT HAY QUE HACER????????
+//*********************import com.google.firebase.quickstart.auth.R;
 
 
 public class EmailPasswordActivity extends AppCompatActivity implements
@@ -13,23 +24,35 @@ public class EmailPasswordActivity extends AppCompatActivity implements
 
 
     private static final String TAG = "EmailPassword";
+
+    //DECLARACIÓN VARIABLES
+    private TextView mStatusTextView;
+    private TextView mDetailTextView;
+    private EditText mEmailField;
+    private EditText mPasswordField;
+
+
     //DECLARAMOS UNA INSTANCIA DE FirebaseAuth
     private FirebaseAuth mAuth;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_emailpassword);
+
         // Views
-
-
-
+        mStatusTextView = findViewById(R.id.status);
+        mDetailTextView = findViewById(R.id.detail);
+        mEmailField = findViewById(R.id.fieldEmail);
+        mPasswordField = findViewById(R.id.fieldPassword);
 
         // Buttons
-
-
-
+        findViewById(R.id.emailSignInButton).setOnClickListener(this);
+        findViewById(R.id.emailCreateAccountButton).setOnClickListener(this);
+        findViewById(R.id.signOutButton).setOnClickListener(this);
+        findViewById(R.id.verifyEmailButton).setOnClickListener(this);
 
         //INICIALIZAMOS LA INSTANCIA DE FirebaseAuth
         mAuth=FirebaseAuth.getInstance();
@@ -48,7 +71,6 @@ public class EmailPasswordActivity extends AppCompatActivity implements
 
 
     //REGISTRAR NUEVOS USUARIOS
-
     //PRIMERO, MÉTODO createAccount QUE RECIBE MAIL Y PASSWORD
     private void createAccount(String email, String password) {
         Log.d(TAG, "createAccount:" + email);
@@ -56,7 +78,7 @@ public class EmailPasswordActivity extends AppCompatActivity implements
             return;
         }
 
-        showProgressDialog();
+        //showProgressDialog();
 
         //SEGUNDO, MÉTODO createUserWithEmailAndPassword PARA CREAR EL USUARIO
         // [START create_user_with_email]
@@ -77,7 +99,7 @@ public class EmailPasswordActivity extends AppCompatActivity implements
                             updateUI(null);
                         }
                         // [START_EXCLUDE]
-                        hideProgressDialog();
+                        //hideProgressDialog();
                         // [END_EXCLUDE]
                     }
                 });
@@ -91,7 +113,7 @@ public class EmailPasswordActivity extends AppCompatActivity implements
         if (!validateForm()) {
             return;
         }
-        showProgressDialog();
+        //showProgressDialog();
 
         // [START sign_in_with_email]
         mAuth.signInWithEmailAndPassword(email, password)
@@ -112,16 +134,52 @@ public class EmailPasswordActivity extends AppCompatActivity implements
                         }
 
                         // [START_EXCLUDE]
-                        if (!task.isSuccessful()) {
-                            mStatusTextView.setText(R.string.auth_failed);
-                        }
-                        hideProgressDialog();
+                        //if (!task.isSuccessful()) {
+                          //  mStatusTextView.setText(R.string.auth_failed);
+                        //}
+                        //hideProgressDialog();
                         // [END_EXCLUDE]
                     }
                 });
         // [END sign_in_with_email]
     }
 
+
+
+
+
+        //MÉTODO PARA HACER EL SIGN OUT
+        private void signOut() {
+                mAuth.signOut();
+                updateUI(null);
+            }
+
+
+
+        //MÉTODO PARA ENVIAR EL EMAIL DE VERIFICACIÓN****************
+    //**************DE MOMENTO NO LO PONEMOS**********************+
+
+
+
+
+
+    //*************************************************************
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//MÉTODO PARA VALIDAR QUE SE HA RELLENADO EL FORM CORRECTAMENTE
     private boolean validateForm() {
         boolean valid = true;
 
@@ -145,6 +203,34 @@ public class EmailPasswordActivity extends AppCompatActivity implements
     }
 
 
+
+
+
+    private void updateUI(FirebaseUser user) {
+        //hideProgressDialog();
+        if (user != null) {
+            //mStatusTextView.setText(getString(R.string.emailpassword_status_fmt,
+              //      user.getEmail(), user.isEmailVerified()));
+            //mDetailTextView.setText(getString(R.string.firebase_status_fmt, user.getUid()));
+
+            findViewById(R.id.emailPasswordButtons).setVisibility(View.GONE);
+            findViewById(R.id.emailPasswordFields).setVisibility(View.GONE);
+            findViewById(R.id.signedInButtons).setVisibility(View.VISIBLE);
+
+            findViewById(R.id.verifyEmailButton).setEnabled(!user.isEmailVerified());
+        } else {
+            //mStatusTextView.setText(R.string.signed_out);
+            mDetailTextView.setText(null);
+
+            findViewById(R.id.emailPasswordButtons).setVisibility(View.VISIBLE);
+            findViewById(R.id.emailPasswordFields).setVisibility(View.VISIBLE);
+            findViewById(R.id.signedInButtons).setVisibility(View.GONE);
+        }
+    }
+
+
+
+
     @Override
     public void onClick(View v) {
         int i = v.getId();
@@ -152,8 +238,14 @@ public class EmailPasswordActivity extends AppCompatActivity implements
             createAccount(mEmailField.getText().toString(), mPasswordField.getText().toString());
         } else if (i == R.id.emailSignInButton) {
             signIn(mEmailField.getText().toString(), mPasswordField.getText().toString());
-        }
+        } else if (i == R.id.signOutButton) {
+            signOut();
+        } //else if (i == R.id.verifyEmailButton) {
+          //  sendEmailVerification();
+        //}
     }
+
+
 
 
 }
--- app/src/main/java/com/example/carlos/firebase_test/MainActivity.java
+++ app/src/main/java/com/example/carlos/firebase_test/MainActivity.java
@@ -2,6 +2,14 @@ package com.example.carlos.firebase_test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
