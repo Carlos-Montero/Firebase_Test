571c4d0866e94f6803e1f503204d7dc429d6366a
Carlos Montero <carlosmonteroreina@gmail.com>
2019-04-24 18:17:07 +0200

    Sexto commit. Funciona escribir datos desde WaterControlActivity. Estamos trabajando en leer datos en TemperatureMonitoringActivity.

--- app/src/main/java/com/example/carlos/firebase_test/view/TemperatureMonitoringActivity.java
+++ app/src/main/java/com/example/carlos/firebase_test/view/TemperatureMonitoringActivity.java
@@ -2,14 +2,59 @@ package com.example.carlos.firebase_test.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import com.example.carlos.firebase_test.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class TemperatureMonitoringActivity extends AppCompatActivity {
+
+    // [START declare_database_ref]
+    private DatabaseReference mDatabase;
+    // [END declare_database_ref]
+
+    //Textview
+    private TextView temperature;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temperature_monitoring);
+
+        //Textview
+        temperature = (TextView) findViewById(R.id.temperature);
+
+        // [START initialize_database_ref]
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        // [END initialize_database_ref]
+
+        //Temperature Listener
+        //**************************************************************************************
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                temperature.setText(value);
+                //Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                //Log.w(TAG, "Failed to read value.", error.toException());
+            }
+
+        });
+
+        //***************************************************************************************
+
     }
 }
--- app/src/main/java/com/example/carlos/firebase_test/view/WaterControlActivity.java
+++ app/src/main/java/com/example/carlos/firebase_test/view/WaterControlActivity.java
@@ -2,9 +2,12 @@ package com.example.carlos.firebase_test.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.example.carlos.firebase_test.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class WaterControlActivity extends AppCompatActivity {
 
@@ -12,9 +15,41 @@ public class WaterControlActivity extends AppCompatActivity {
     private Button lessWater;
     private Button moreWater;
 
+    // [START declare_database_ref]
+    private DatabaseReference mDatabase;
+    // [END declare_database_ref]
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_water_control);
+
+        // [START initialize_database_ref]
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        // [END initialize_database_ref]
+
+
+        //Buttons
+        lessWater = (Button) findViewById(R.id.lessWater);
+        moreWater = (Button) findViewById(R.id.moreWater);
+
+
+        //onClickListeners
+        lessWater.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDatabase.child("message").setValue("Less Water");
+            }
+        });
+
+        moreWater.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDatabase.child("message").setValue("More Water");
+            }
+        });
+
+
+
     }
 }
